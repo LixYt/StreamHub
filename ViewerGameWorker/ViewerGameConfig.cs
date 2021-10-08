772891cfa6fa65ff7af4d928f5cc0db78e91128b
Lix <lix.l.ancien@live.fr>
2021-10-08 22:33:39 +0200

    Add Broadcaster token

--- ViewerGameWorker/ViewerGameConfig.cs
+++ ViewerGameWorker/ViewerGameConfig.cs
@@ -9,19 +9,28 @@ namespace ViewerGameWorker
 {
     class ViewerGameConfig
     {
-        public string ClientID { get; set; } = "";
         public string ChannelName { get; set; } = "";
-        public string AccessToken { get; set; } = "";
-        public string RefreshToken { get; set; } = "";
-        
+        public string Bot_ClientID { get; set; } = "";
+        public string Bot_AccessToken { get; set; } = "";
+        public string Bot_RefreshToken { get; set; } = "";
+        public string BroadCaster_ClientID { get; set; } = "";
+        public string BroadCaster_AccessToken { get; set; } = "";
+        public string BroadCaster_RefreshToken { get; set; } = "";
+
         public string CommandSymbol { get; set; } = "#";
         public string BotName { get; set; } = "";
         public string DbString { get; set; } = "Data Source=SERVER;Initial Catalog=DATABASE;Integrated Security=False;user id=USER;password=PASSWORD";
         public string GameTitle { get; set; } = "Game Name";
-        public ConnectionCredentials GetCredential()
+        
+        
+        
+        public ConnectionCredentials GetBotCredential()
         {
-            return new ConnectionCredentials(ChannelName, AccessToken);
+            return new ConnectionCredentials(ChannelName, Bot_AccessToken);
         }
-
+        public ConnectionCredentials GetCasterCredential()
+        {
+            return new ConnectionCredentials(ChannelName, Bot_AccessToken);
+        }
     }
 }
